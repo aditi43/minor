00bc8781d001d33c3c15da063260803870651ed9
ADITI MISHRA <105098382+aditi43@users.noreply.github.com>
2022-12-07 16:53:18 +0530

    Initial commit

---
+++ course_pages/testJavaScript/questions.js
@@ -0,0 +1,97 @@
+let questions = [
+  {
+    numb: 1,
+    question:
+      "Which of the following data type is supported in C++ but not in C?",
+    answer: "bool",
+    options: ["int", "float", "bool", "double"]
+  },
+  {
+    numb: 2,
+    question:
+      "By which of the following can the if-else statement be replaced?",
+    answer: "Conditional Operator",
+    options: [
+      "Bitwise Operator",
+      "Logical Operator",
+      "Conditional Operator",
+      "Arithmetic Operator"
+    ]
+  },
+  {
+    numb: 3,
+    question: "How much bytes of memory does void occupy?",
+    answer: "0",
+    options: ["1", "0", "2", "4"]
+  },
+  {
+    numb: 4,
+    question: "Total types of constructors in C++ are?",
+    answer: "3",
+    options: ["1", "2", "3", "4"]
+  },
+  {
+    numb: 5,
+    question:
+      "Data members and member functions of a class are private by default.True or False?",
+    answer: "True",
+    options: ["True", "False", "Depends on the code", "None"]
+  },
+  {
+    numb: 6,
+    question: `Find the output of the following program.
+        main(){<br>
+         int i = (1, 2, 3);<br>
+         cout << i << endl;<br>
+        }`,
+    answer: 3,
+    options: ["1", "2", "3", "Error"]
+  },
+  {
+    numb: 7,
+    question: `Find the output of the following program.
+
+        main(){<br>
+          int a = 10, b, c;<br>
+          b  = a++;<br>
+          c = a;<br>
+          cout << a << “ “ <<b <<” “<< c << endl;<br>
+        }`,
+    answer: "11 10 11",
+    options: ["10 11 11", "11 11 11", "11 10 11", "10 10 10"]
+  },
+  {
+    numb: 8,
+    question: `Choose the size of the below struct.
+
+        Struct{<br>
+        Int a;<br>
+        Char b;<br>
+        Float c;<br>
+        }`,
+    answer: 7,
+    options: ["2", "4", "7", "10"]
+  },
+  {
+    numb: 9,
+    question: `Find the output of the following program.
+
+        main(){<br>
+         char ch[] = “Interviewbit Scaler”; <br>
+         int l = strlen(ch);<br>
+         cout << l << endl;<br>
+        }`,
+    answer: 19,
+    options: ["18", "19", "20", "21"]
+  },
+  {
+    numb: 10,
+    question: `Find the output of the following program.
+
+        main(){<br>
+         cout <<  -10 - 10 -10;<br>
+        }`,
+    answer: -30,
+    options: ["0", "-30", "30", "20"]
+  }
+];
